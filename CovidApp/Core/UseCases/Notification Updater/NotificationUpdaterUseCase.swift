86b1e98235427dfd910c592d4983aefbf0b4b042
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2020-10-14 23:48:46 +0400

    Create initial files for UseCases

---
+++ CovidApp/Core/UseCases/Notification Updater/NotificationUpdaterUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationUpdaterUseCase.swift
+//  CovidApp
+//
+//  Created by Giorgi Kratsashvili on 10/14/20.
+//
+
+import Foundation
---
+++ CovidApp/Core/UseCases/Summaries Fetching/SummariesFetchingUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  SummariesFetchingUseCase.swift
+//  CovidApp
+//
+//  Created by Giorgi Kratsashvili on 10/14/20.
+//
+
+import Foundation
